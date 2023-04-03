f5988f88ae5596cd4d3cb5be0179a6c5d52417c2
skhapijulhossen <skhapijulhossen@gmail.com>
2023-04-03 06:05:53 +0530

    logestSubarrayWithSumK

--- logestSubarrayWithSumK/main.py
+++ logestSubarrayWithSumK/main.py
@@ -2,21 +2,27 @@ from typing import List
 
 
 def get_subarray_sum_k(arr: List[int], k: int) -> int:
-    if len(arr)<2 or k<0:
+    """
+    ### Time: O(n)    Space: O(1)
+    """
+    if len(arr) < 2 or k < 0:
         return -1
-    
+
     maxlen = 0
     p1, p2 = 0, 1
     summ = arr[p1]
-    
-    while p2<len(arr):
+
+    while p2 < len(arr):
         summ += arr[p2]
-        if not sum^k and (p2 - p1 + 1) > maxlen:
+        # when summ equals and len is max, update maxlen of sub-array
+        if (not summ ^ k) and ((p2 - p1 + 1) > maxlen):
             maxlen = p2 - p1 + 1
-        elif summ>k:
+        # when summ gets greater than target k, shrink and subtract
+        elif summ > k:
+            summ -= arr[p1]
             p1 += 1
         p2 += 1
-
+    del summ
     return maxlen
 
 
@@ -25,8 +31,15 @@ if __name__ == '__main__':
     # out = 4
     # array = (1,1,2,3,1,4,2,1)     k=4
     # out = 3
-    # array = (1,1,2,3,2,1, 2)    k=5
-    # out = 4
-    arrays = [((1,1,2,3,1,4,2,1), 7), ((1,1,2,3,1,4,2,1), 4), ((1,1,2,3,2,1, 2), 5)]
+    # array = (4, 2, 3, 2, 1, 2)    k=5
+    # out = 3
+    # array = (4, 2, 3, 2, 1, 2)    k=8
+    # out = 3
+    arrays = [
+        ((1, 1, 2, 3, 1, 4, 2, 1), 7), ((1, 1, 2, 3, 1, 4, 2, 1), 4),
+        ((4, 2, 3, 2, 1, 2), 5), ((4, 2, 3, 2, 1, 2), 8)
+    ]
+    # Test
     for arr, k in arrays:
         lenth = get_subarray_sum_k(arr, k)
+        print(f'array={arr} & k={k}:{lenth}')
