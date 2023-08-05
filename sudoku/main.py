210dbf4408e6c45c79ee504c29cd6294acc44a6d
skhapijulhossen <skhapijulhossen@gmail.com>
2023-08-05 08:35:13 +0530

    Sudoku validator | non-recursive solution

---
+++ sudoku/main.py
@@ -0,0 +1,94 @@
+
+"""36. Valid Sudoku
+Medium
+9.3K
+965
+Companies
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+Accepted
+1.2M
+Submissions
+2M
+Acceptance Rate
+58.5%"""
+
+from typing import List
+from collections import defaultdict
+
+
+def isValidSudoku(board: List[List[str]]) -> bool:
+    rows = defaultdict(set)         # to store current row elements
+    cols = defaultdict(set)         # to store current column elements
+    subBoard = defaultdict(set)     # to store subBoard elements
+
+    # test rules
+    one, two, three = (0, 3), (3, 6), (6, 9)
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == '.':
+                continue
+
+            # check rules
+            if (board[row][col] in rows[row]) or (board[row][col] in cols[col]) or (board[row][col] in subBoard[(row//3, col//3)]):
+                return False
+
+            # udate stores
+            rows[row].add(board[row][col])
+            cols[col].add(board[row][col])
+            subBoard[(row//3, col//3)].add(board[row][col])
+    return True
+
+
+board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8",
+                                                                                                                                                                                                       ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+board2 = [["8", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".",
+                                                                                                                                                                                                       "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+print(isValidSudoku(board1))
+print(isValidSudoku(board2))
