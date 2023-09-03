226605662cbf109bb972f1ee006ee1df2cb11086
skhapijulhossen@gmail <skhapijulhossen@gmail.com>
2023-09-03 09:34:36 +0530

    Bulbswitcher I | O(1) | O(1)

---
+++ BulbSwitcher/bulbswitchI.py
@@ -0,0 +1,48 @@
+"""319. Bulb Switcher
+Medium
+2.4K
+3K
+Companies
+There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+
+On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb.
+
+Return the number of bulbs that are on after n rounds.
+
+ 
+
+Example 1:
+
+
+Input: n = 3
+Output: 1
+Explanation: At first, the three bulbs are [off, off, off].
+After the first round, the three bulbs are [on, on, on].
+After the second round, the three bulbs are [on, off, on].
+After the third round, the three bulbs are [on, off, off]. 
+So you should return 1 because there is only one bulb is on.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+0 <= n <= 109
+Accepted
+199.5K
+Submissions
+379.7K
+Acceptance Rate
+52.5%"""
+
+
+import math
+
+def bulbSwitch(n: int) -> int:
+    return int(math.sqrt(n))
